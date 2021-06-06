adf07e5a8bd5eebf57737186ac58ca5be2723845
Komikow Sergey <komikow@mail.ru>
2021-06-06 18:37:20 +0300

    add task Classes

---
+++ src/by/gsu/epam/Car.java
@@ -0,0 +1,85 @@
+package by.gsu.epam;
+
+public class Car {
+    private int id;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private String number;
+
+    public Car() {
+    }
+
+    public Car(int id, String brand, String model, int year, String color, int price, String number) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id + ", brand = " + brand + ", model = " + model + ", year = " + year + ", color = " + color + ", price = " + price + ", number = " + number;
+    }
+}
